c4717b8b9d493c27b112dfb24a4d4eeffd960f9c
jianfe66 <jianfe66@gmail.com>
2022-12-09 19:20:06 +0100

    Video 18 curso gas-avanzado

--- funciones.js
+++ funciones.js
@@ -1,18 +1,26 @@
 const HOJA = SpreadsheetApp.openById('1DyTlqwVCeKIpSlT2d75F3yHXuMjVRCTcBrBHP3b-LqM').getActiveSheet();
 
-function doGet(){
+function doGet()
+{
     return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda Google Apps Script');
 }
 
-function obtenerDatosHTML(nombre){
+function obtenerDatosHTML(nombre)
+{
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
 
-function obtenerContactos(){
+function obtenerContactos()
+{
     return HOJA.getDataRange().getValues();
 }
 
 function insertarContacto(nombre, apellidos,correo,telf)
 {
     HOJA.appendRow([nombre, apellidos,correo,telf]);
+}
+
+function borrarContacto(numFila)
+{
+    HOJA.deleteRow(numFila);
 }
